f656a7a21dcc4e2a15f6538462b2e8fbbb19c91c
Toksmajek <167389697+Toksmajek@users.noreply.github.com>
2026-09-17 16:51:21 -0600

    Fix and run beginner Python course exercises

--- python-course-one/python_course_one.py
+++ python-course-one/python_course_one.py
@@ -1,42 +1,35 @@
- PRIMES = [2, 3, 5, 7, 11
- 
- for prime in primes: 
-     print (f' {prime} is a prime number.')]
- 
-     
- kids_ages = (3, 7, 12)
- for age in kid_ages: 
-     print (f'I have a {age} year old kid. ')
-     
-         
-             
- for i in range (20): 
-     print(i)    Write your Python course exercises here.
+# Python course one - practice through Video 7.
+
+primes = [2, 3, 5, 7, 11]
+for prime in primes:
+    print(f'{prime} is a prime number.')
+
+kids_ages = (3, 7, 12)
+for age in kids_ages:
+    print(f'I have a {age} year old kid.')
+
+for i in range(20):
+    print(i)
 
 my_friends = {
-    'Jose' : 6,
-    'Rolf' : 12,
-    'Anne' : 6
+    'Jose': 6,
+    'Rolf': 12,
+    'Anne': 6
 }
 
-for name in my_friends.items() 
-    print(f'I last saw {name} {my_friends[name]} days ago
-    
-    
-print(my_friends.items())   # [('Jose' , 6), ('Rolf' , 12), ('Anne' , 6)]
+for name in my_friends:
+    print(f'I last saw {name} {my_friends[name]} days ago')
 
-for t in [('Jose' , 6), ('Rolf' , 12), ('Anne' , 6)]:
+print(my_friends.items())
+
+for t in [('Jose', 6), ('Rolf', 12), ('Anne', 6)]:
     n, v = t
     print(n)
     print(v)
-    
-for name, days in my_friends.items(): 
-    if name == 'Rolf': 
-    print('I know Rolf!')
-    
-    
-who_do_i_know in my_friends: 
-    print('I also know Anne')
-    
-    
 
+for name, days in my_friends.items():
+    if name == 'Rolf':
+        print('I know Rolf!')
+
+if 'Anne' in my_friends:
+    print('I also know Anne')
