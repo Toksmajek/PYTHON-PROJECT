0263c94920fef8b992fb486d9f4d9e01215fe55d
Toksmajek <167389697+Toksmajek@users.noreply.github.com>
2026-09-17 16:49:50 -0600

    Save Python course one practice through Video 7

---
+++ python-course-one/python_course_one.py
@@ -0,0 +1,42 @@
+ PRIMES = [2, 3, 5, 7, 11
+ 
+ for prime in primes: 
+     print (f' {prime} is a prime number.')]
+ 
+     
+ kids_ages = (3, 7, 12)
+ for age in kid_ages: 
+     print (f'I have a {age} year old kid. ')
+     
+         
+             
+ for i in range (20): 
+     print(i)    Write your Python course exercises here.
+
+my_friends = {
+    'Jose' : 6,
+    'Rolf' : 12,
+    'Anne' : 6
+}
+
+for name in my_friends.items() 
+    print(f'I last saw {name} {my_friends[name]} days ago
+    
+    
+print(my_friends.items())   # [('Jose' , 6), ('Rolf' , 12), ('Anne' , 6)]
+
+for t in [('Jose' , 6), ('Rolf' , 12), ('Anne' , 6)]:
+    n, v = t
+    print(n)
+    print(v)
+    
+for name, days in my_friends.items(): 
+    if name == 'Rolf': 
+    print('I know Rolf!')
+    
+    
+who_do_i_know in my_friends: 
+    print('I also know Anne')
+    
+    
+
