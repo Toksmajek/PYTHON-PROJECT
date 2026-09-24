89dc49ec5309342e729b265891b6d46b4e19c236
Toksmajek <167389697+Toksmajek@users.noreply.github.com>
2026-09-24 10:14:29 -0600

    Save list slicing course notes

---
+++ python-course-one/list_slicing.py
@@ -0,0 +1,18 @@
+List slicing in Python
+
+friends =  ['rolf', 'bob', 'jen', 'anne', 'mary']
+print(friends[2:4])
+
+print(friends[-3:-1])
+  
+print(friends[-3:])
+
+print(friends[:2]) 
+
+print(friends[:-2])
+
+my_table[1:]
+
+
+
+ # prints from index 1 to the end
